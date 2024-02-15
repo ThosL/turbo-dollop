5075f5952bfd6c26e14096d9117f7c070dc360b7
tomle <thomasle993@gmail.com>
2024-02-14 18:34:29 -0600

    Added Main Project And sln, plus added Class lib for JAT

---
+++ JATLib/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JATLib
+{
+    public class Application
+    {
+        public int ApplicationID { get; set; }
+        public int FK_JobCompanyID { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public int ApplicationSourceID { get; set; }
+        public int ApplicationURLID { get; set; }
+        public int JobPostingURLID { get; set; }
+        public int SalaryRangeID { get; set; }
+
+    }
+}
